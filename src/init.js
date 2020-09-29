74592e7eebba8c0736cb0527f4dd278625a0a0b4
David Acosta <acosta816@gmail.com>
2020-09-28 20:34:53 -0700

    completed html sort of, next: add CSS

---
+++ src/init.js
@@ -0,0 +1,27 @@
+import game from './gameState';
+
+
+
+const TICK_RATE = 3000;
+
+async function init() {
+  console.log("starting game");
+
+  let nextTimeToTick = Date.now();
+
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      game.tick();
+      nextTimeToTick = now + TICK_RATE;
+    };
+
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  requestAnimationFrame(nextAnimationFrame);
+};
+
+
+init();
